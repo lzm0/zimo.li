22f9ed8a1ace6ef08e1f8735dfcf707a67b2e769
Zimo Li <7163127+lzm0@users.noreply.github.com>
2023-10-10 20:00:04 -0400

    Update styles

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen antialiased max-w-2xl my-20 mx-6 md:mx-auto">
       <LanguageToggle lang={lang} />
-      <section className="prose prose-neutral dark:prose-invert my-20">
+      <section className="prose prose-neutral dark:prose-invert my-16">
         {lang === "en" ? <English /> : <Chinese />}
       </section>
       <Terminal />
@@ -33,13 +33,15 @@ function getLanguage() {
 function English() {
   return (
     <>
-      <h1 className="font-semibold text-2xl tracking-tight">
+      <h1 className="font-semibold text-2xl tracking-tight mb-16">
         Hi there, I'm Zimo <WavingHand />
       </h1>
       <p>
         I'm a <a href="https://github.com/lzm0">software engineer</a> based in
-        Toronto. I enjoy solving complex problems through simple, elegant
-        solutions while learning new things along the way. Find me on{" "}
+        Toronto. I enjoy solving complex problems with simple solutions while
+        learning new things along the way. I also{" "}
+        <a href="https://blog.zimo.li">blog</a> about technical stuff that I
+        find interesting. Find me on{" "}
         <a href="https://www.linkedin.com/in/li-zimo/">LinkedIn</a> if you want
         to get in touch.
       </p>
@@ -51,12 +53,14 @@ function English() {
 function Chinese() {
   return (
     <>
-      <h1 className="font-semibold text-2xl tracking-tight">
+      <h1 className="font-semibold text-2xl tracking-tight mb-16">
         你好，我是子沫 <WavingHand />
       </h1>
       <p>
-        我是一名在多伦多的<a href="https://github.com/lzm0">软件工程师</a>
-        。欢迎通过
+        我是一名在多伦多的<a href="https://github.com/lzm0">软件工程师</a>。
+        我喜欢通过简洁的方案来解决复杂的问题，并在此过程中学习新的东西。
+        我也会在我的<a href="https://blog.zimo.li">博客</a>
+        上写一些我觉得有趣的话题。 欢迎通过
         <a href="https://www.linkedin.com/in/li-zimo/">领英</a>
         联系我。
       </p>
