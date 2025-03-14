664ba7ced4d80f605f6575e3aa26af32ce9b1bfc
Zimo Li <7163127+lzm0@users.noreply.github.com>
2025-03-14 15:30:46 -0400

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const sans = DM_Sans({ subsets: ["latin"], display: "block" });
 
@@ -23,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={sans.className + " scroll-smooth"}>
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
